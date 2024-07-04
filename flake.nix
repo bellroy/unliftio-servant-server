d78a86c25b7006c6d2dc63ddd13b043c5265cc3e
Jack Kelly <jack.kelly@bellroy.com>
2024-07-05 08:05:48 +1000

    Use our simplified flake

--- flake.nix
+++ flake.nix
@@ -5,17 +5,14 @@
 
   outputs = inputs:
     inputs.bellroy-nix-foss.lib.haskellProject {
-      cabalPackages = [
-        {
-          name = "unliftio-servant-server";
-          path = ./unliftio-servant-server.nix;
-        }
+      supportedCompilers = [
+        "ghc810"
+        "ghc90"
+        "ghc92"
+        "ghc94"
+        "ghc96"
+        "ghc98"
       ];
-      supportedCompilers = [ "ghc810" "ghc90" "ghc92" "ghc94" "ghc96" ];
-      defaultCompiler = "ghc92";
-      haskellPackagesOverride = { haskellLib, prev, ... }: {
-        servant = haskellLib.doJailbreak prev.servant;
-        servant-server = haskellLib.doJailbreak prev.servant-server;
-      };
+      defaultCompiler = "ghc96";
     };
 }
