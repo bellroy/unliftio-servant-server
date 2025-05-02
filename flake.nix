e6196bc121132db699d71cd825e3412144783416
Michael Webb <michaelwebb76@gmail.com>
2025-05-02 14:41:55 +1000

    Support a new GHC

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
     bellroy-nix-foss.url = "github:bellroy/bellroy-nix-foss";
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.bellroy-nix-foss.lib.haskellProject {
       src = ./.;
       supportedCompilers = [
@@ -14,6 +15,7 @@
         "ghc96"
         "ghc98"
         "ghc910"
+        "ghc912"
       ];
       defaultCompiler = "ghc96";
     };
