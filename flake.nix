ab51f3bab88ca98e9f8c22432c707c35aa95c22c
Jack Kelly <jack.kelly@bellroy.com>
2026-02-17 05:19:38 +0000

    flake.nix: Drop GHCs no longer provided by nixpkgs

--- flake.nix
+++ flake.nix
@@ -8,9 +8,6 @@
     inputs.bellroy-nix-foss.lib.haskellProject {
       src = ./.;
       supportedCompilers = [
-        "ghc810"
-        "ghc90"
-        "ghc92"
         "ghc94"
         "ghc96"
         "ghc98"
